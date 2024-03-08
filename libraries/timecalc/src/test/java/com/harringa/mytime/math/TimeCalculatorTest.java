da9b27cf80eb9bd8bdb75b0a7988208857ba8ad9
Justin Harringa <justin@harringa.com>
2024-03-08 01:36:43 -0800

    Use hamcrest instead of junit assertThat

--- libraries/timecalc/src/test/java/com/harringa/mytime/math/TimeCalculatorTest.java
+++ libraries/timecalc/src/test/java/com/harringa/mytime/math/TimeCalculatorTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TimeCalculatorTest {
 
